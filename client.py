34746c3ddf69263b6d5eef21bc2d764d04655373
Natounet <70477133+Natounet@users.noreply.github.com>
2021-07-04 13:06:25 +0200

    Revert "Changement système affichage des messages"
    This reverts commit a27d44250fd4669e49219dfa03b322262b0149bd.

--- client.py
+++ client.py
@@ -104,7 +104,7 @@ while True:
             # Quand on reçoit un message
             if sockets == s:
                 message = receive(s)
-                print(message)
+                liste_message += (message)
                 affichage()
                 
 
